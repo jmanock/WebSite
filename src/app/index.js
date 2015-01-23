33952faaf2c84c6899f6d1fc52eee1a9a8491e49
jmanock <jonmanock@gmail.com>
2015-01-22 20:47:32 -0500

    splash page started

--- src/app/index.js
+++ src/app/index.js
@@ -6,7 +6,46 @@ angular.module('webSite', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'n
       .state('home', {
         url: '/',
         templateUrl: 'app/main/main.html',
-        controller: 'MainCtrl'
+        controller: 'MainCtrl',
+        data:{
+          css:'app/main/main.css'
+        }
+      })
+
+      .state('resume',{
+        url:'/resume',
+        templateUrl: 'app/resume/resume.html',
+        controller: 'ResumeCtrl',
+        data:{
+          css:'app/resume/resume.css'
+        }
+      })
+
+      .state('contact', {
+        url:'/contact',
+        templateUrl: 'app/contact/contact.html',
+        controller: 'ContactCtrl',
+        data:{
+          css:'app/contact/contact.css'
+        }
+      })
+
+      .state('games',{
+        url:'/games',
+        templateUrl: 'app/games/games.html',
+        controller: 'GamesCtrl',
+        data:{
+          css:'app/games/games.css'
+        }
+      })
+
+      .state('portfolio',{
+        url:'/portfolio',
+        templateUrl:'app/portfolio/portfolio.html',
+        controller:'PortfolioCtrl',
+        data:{
+          css:'app/portfolio/portfolio.css'
+        }
       });
 
     $urlRouterProvider.otherwise('/');
